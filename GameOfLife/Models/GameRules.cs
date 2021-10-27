1de525bd9265a7941558f5d895c2e4ce1580f08e
johanastrom <johan.astrom89@gmail.com>
2021-10-27 16:28:12 +0200

    Refactoring..

---
+++ GameOfLife/Models/GameRules.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Models
+{
+    public class GameRules
+    {
+        public bool GetStateByCoordinates(Position coordinates, IImmutableList<Node> nodes)
+        {
+            return nodes.SingleOrDefault(n => n.Coordinates.Equals(coordinates)) is not null;
+        }
+
+        public IImmutableList<Node> Parse(string[] coordinates)
+        {
+            var nodes = new List<Node>();
+            Array.ForEach(coordinates,
+                c => AddNode(nodes,
+                Array.ConvertAll(c.Split(','), int.Parse)));
+            return nodes.ToImmutableList();
+        }
+
+        private static void AddNode(List<Node> nodes, int[] splitCoordinates)
+        {
+            nodes.Add(new Node
+            {
+                Coordinates = new Position(splitCoordinates[0], splitCoordinates[1]),
+            });
+        }
+
+        public int FindMaxXCoordinate(IImmutableList<Node> nodes)
+        {
+            return nodes.Select(n => n.Coordinates.XCoordinate).Max();
+        }
+
+        public int FindMaxYCoordinate(IImmutableList<Node> nodes)
+        {
+            return nodes.Select(n => n.Coordinates.YCoordinate).Max();
+        }
+
+        public IImmutableList<Node> ComputeGeneration(IImmutableList<Node> nodes)
+        {
+            var nextGeneration = new List<Node>(nodes);
+            for (int x = 1; x <= FindMaxXCoordinate(nodes); x++)
+            {
+                for (int y = 1; y <= FindMaxYCoordinate(nodes); y++)
+                {
+                    ComputeSurvival(new GenerationData(nodes, nextGeneration, new Position(x, y)));
+                }
+            }
+            return nextGeneration.ToImmutableList<Node>();
+        }
+
+        private void ComputeSurvival(GenerationData generationData)
+        {
+            int livingNeighbours = CountLivingNeighbours(generationData);
+
+            if (GetStateByCoordinates(generationData.Coordinates, generationData.LivingNodes) && (livingNeighbours > 3 || livingNeighbours < 2))
+            {
+                RemoveAtCoordinates(generationData);
+            }
+            else if (!GetStateByCoordinates(generationData.Coordinates, generationData.LivingNodes) && livingNeighbours == 3)
+            {
+                generationData.NextGeneration.Add(new Node { Coordinates = generationData.Coordinates });
+            }
+        }
+
+        private int CountLivingNeighbours(GenerationData generationData)
+        {
+            int livingNeighbours = 0;
+            generationData.Neighbours.Values.ToList().ForEach(neighbour =>
+                livingNeighbours += GetStateByCoordinates(neighbour, generationData.LivingNodes) ? 1 : 0);
+
+            return livingNeighbours;
+        }
+
+        private static bool RemoveAtCoordinates(GenerationData generationData)
+        {
+            var node = generationData.LivingNodes.SingleOrDefault(n => n.Coordinates.Equals(generationData.Coordinates));
+            return generationData.NextGeneration.Remove(node);
+        }
+    }
+}
---
+++ GameOfLife/Models/GenerationData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Models
+{
+    public class GenerationData
+    {
+        public IImmutableList<Node> LivingNodes { get; set; }
+        public List<Node> NextGeneration { get; set; }
+        public Position Coordinates { get; set; }
+        public ImmutableDictionary<String, Position> Neighbours { get; }
+
+        public GenerationData(IImmutableList<Node> livingNodes, List<Node> nextGeneration, Position coordinates)
+        {
+            LivingNodes = livingNodes;
+            NextGeneration = nextGeneration;
+            Coordinates = coordinates;
+            Neighbours = new Dictionary<string, Position>()
+            {
+                {"UpperLeft", new Position(Coordinates.XCoordinate - 1, Coordinates.YCoordinate + 1) },
+                {"UpperCenter", new Position(Coordinates.XCoordinate, Coordinates.YCoordinate + 1) },
+                {"UpperRight", new Position(Coordinates.XCoordinate + 1, Coordinates.YCoordinate + 1) },
+                {"Left", new Position(Coordinates.XCoordinate - 1, Coordinates.YCoordinate) },
+                {"Right", new Position(Coordinates.XCoordinate + 1, Coordinates.YCoordinate) },
+                {"LowerLeft", new Position(Coordinates.XCoordinate - 1, Coordinates.YCoordinate - 1) },
+                {"LowerCenter", new Position(Coordinates.XCoordinate, Coordinates.YCoordinate - 1) },
+                {"LowerRight", new Position(Coordinates.XCoordinate + 1, Coordinates.YCoordinate - 1) }
+            }.ToImmutableDictionary();
+    }
+
+
+    }
+}
+    
