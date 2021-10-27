74097b549622f16dcac9ce3126dea2558cbfcb1f
johanastrom <johan.astrom89@gmail.com>
2021-10-27 10:43:20 +0200

    Added SurvivalItem class

---
+++ GameOfLife/Models/SurvivalItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Models
+{
+    public class SurvivalItem
+    {
+        public ImmutableList<Node> LivingNodes { get; set; }
+        public List<Node> NextGeneration { get; set; }
+        public Position Coordinates { get; set; }
+        public List<Position> Neighbours { get; set; }
+        public SurvivalItem()
+        {
+            Neighbours = new()
+            {
+                new Position(Coordinates.XCoordinate - 1, Coordinates.YCoordinate + 1),
+                new Position(Coordinates.XCoordinate, Coordinates.YCoordinate + 1),
+                new Position(Coordinates.XCoordinate + 1, Coordinates.YCoordinate + 1),
+                new Position(Coordinates.XCoordinate - 1, Coordinates.YCoordinate),
+                new Position(Coordinates.XCoordinate + 1, Coordinates.YCoordinate),
+                new Position(Coordinates.XCoordinate - 1, Coordinates.YCoordinate - 1),
+                new Position(Coordinates.XCoordinate, Coordinates.YCoordinate - 1),
+                new Position(Coordinates.XCoordinate + 1, Coordinates.YCoordinate - 1),
+            };
+        }
+    }
+}
