6c688851289a500a43a31bc214cc6deca1295c5a
johanastrom <johan.astrom89@gmail.com>
2021-10-28 08:01:10 +0200

    Added GameController.cs

---
+++ GameOfLife/Controllers/GameController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Controllers
+{
+    public class GameController
+    {
+        private readonly ILogger<GameController> _logger;
+
+        public GameController(ILogger<GameController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
