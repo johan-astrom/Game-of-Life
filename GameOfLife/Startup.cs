e9cc1141fad9e99b27a57b5c0d9da16760abcc2c
johanastrom <johan.astrom89@gmail.com>
2021-11-02 16:00:14 +0100

    Set default endpoint to Game/Index

--- GameOfLife/Startup.cs
+++ GameOfLife/Startup.cs
@@ -50,7 +50,7 @@ namespace GameOfLife
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Game}/{action=Index}/{id?}");
             });
         }
     }
